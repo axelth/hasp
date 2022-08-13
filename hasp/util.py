f92886ba4866236a0e17e9d4aea3f3988b5414b7
Carl Quin <89825626+carlq89@users.noreply.github.com>
2022-08-13 15:45:59 +0900

    corection on code

--- hasp/util.py
+++ hasp/util.py
@@ -5,6 +5,6 @@ def combine_classes(target_list, pred_arr):
     target_list: the list of intergers of desired target class e.g. [1, 8]
     pred_arr: np.array, an array of the values from prediction
     '''
-    new_num = max(pred_arr)+1
-    new_arr = np.where(np.isin(pred_arr, target_list), pred_arr, 10)
+    new_num = max(target_list)-1
+    new_arr = np.where(np.isin(pred_arr, target_list), pred_arr, new_num)
     return new_arr
