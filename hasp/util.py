137f0a1115f6b262b29a1ff73ec178cae12dcc39
Axel Theorin <axel.theorin@gmail.com>
2022-08-20 14:05:38 +0900

    Add binary class option to combine_classes and update notebook

--- hasp/util.py
+++ hasp/util.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 
-def combine_classes(target_list, pred_arr):
+def combine_classes(target_list, label_arr, binary=False):
     '''
     target_list: the list of intergers of desired target class e.g. [1, 8]
     pred_arr: np.array, an array of the values from prediction
     '''
-    new_num = max(target_list)-1
-    new_arr = np.where(np.isin(pred_arr, target_list), pred_arr, new_num)
-    return new_arr
+    if binary:
+        return np.where(np.isin(label_arr, target_list), 1, 0)
+    else:
+        new_num = max(target_list)-1
+        new_arr = np.where(np.isin(label_arr, target_list), label_arr, new_num)
+        return new_arr
 
 
 def np_pad_wrapper(arrays, max_len=64000):
